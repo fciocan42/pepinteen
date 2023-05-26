76a2e5ed9470da87698c6421a20ed5d9ca0a82a6
Ciocan Florin <ciocanflorin37@yahoo.com>
2023-05-26 10:45:28 +0300

    Node JS setup

---
+++ JS-Server/nvm_install.sh
@@ -0,0 +1,24 @@
+echo INSTALL NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh |
+bash
+
+echo
+echo SOURCE PROFILE
+# restart de terminal
+source ~/.bash_profile
+
+echo
+echo INSTALL LTS
+nvm install --lts
+
+echo
+echo USE LTS
+nvm use --lts
+
+echo
+echo NODE VERSION
+node –version
+
+echo
+echo NPM VERSION
+npm –version
