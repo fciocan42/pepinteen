0023c321c435b0311e0c56c2072d04ae96c31aeb
Ciocan Florin <ciocanflorin37@yahoo.com>
2023-05-26 18:11:44 +0300

    postman
    Delete postman-linux-x64.tar.gz
    
    bash
    
    postman sh

--- JS-Server/nvm_install.sh
+++ JS-Server/nvm_install.sh
@@ -8,5 +8,33 @@ echo SOURCE PROFILE
 # restart de terminal
 source ~/.bashrc
 
-reset
-bash nvm_use.sh
+export NVM_DIR=$HOME/.nvm;
+source $NVM_DIR/nvm.sh;
+
+
+echo
+echo INSTALL LTS
+nvm install --lts
+
+echo
+echo USE LTS
+nvm use --lts
+
+echo
+echo NODE VERSION
+node --version
+
+echo
+echo NPM VERSION
+npm -version
+
+echo
+echo POSTMAN
+postmanDir=~/Downloads
+postmanTar=~/Downloads/postman.tar.gz
+
+curl https://dl.pstmn.io/download/latest/linux_64 --output $postmanTar
+
+tar -xf $postmanTar -C $postmanDir
+
+ln -sv $postmanDir/Postman/Postman ~/Desktop
